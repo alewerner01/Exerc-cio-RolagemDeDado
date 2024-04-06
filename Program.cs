fc8ec54ace52b3bc4bca616d8b8e3d8c62f73868
alewerner01 <162760202+alewerner01@users.noreply.github.com>
2024-04-05 22:17:46 -0300

    Programa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Cryptography;
+
+int faces, sorteado;
+
+Console.WriteLine("--- Rolagem de Dado --- \n");
+
+Console.WriteLine("Quantas faces? ");
+faces = Convert.ToInt32(Console.ReadLine());
+
+sorteado = RandomNumberGenerator.GetInt32(1, faces + 1);
+
+Console.WriteLine($"\nRolagem: {sorteado}");
+
+
